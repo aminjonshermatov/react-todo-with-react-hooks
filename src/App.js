c4a2b10532404085062aa46266d071b22fd17a51
Aminjon <aminjonshermatov@gmail.com>
2020-11-01 00:44:11 +0500

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useReducer} from 'react';
+import TodoList from './TodoList';
+import {Context} from './context';
+import reducer from './reducer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  const [state, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem('todos')));
+
+  const [todoTitle, setTodoTitle] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(state));
+  }, [state]);
+
+  const addTodo = ev => {
+    if (ev.key === 'Enter') {
+      dispatch({
+        type: 'add',
+        payload: todoTitle
+      });
+      setTodoTitle('');
+    }
+  };
+
+  /* const removeTodo = id => {
+    setTodos(todos.filter(item => {
+      return item.id !== id;
+    }));
+  };
+
+  const toggleTodo = id => {
+    setTodos(todos.filter(item => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+
+      return item;
+    }));
+  }; */
+
+    return (
+      <Context.Provider value={{
+        dispatch
+      }}>
+        <div className="container">
+          <h1>Todo app</h1>
+
+            <div className="input-field">
+              <input
+                type="text"
+                value={todoTitle}
+                onChange={ev => setTodoTitle(ev.target.value)}
+                onKeyPress={addTodo}
+              />
+              <label>Todo name</label>
+            </div>
+
+            <TodoList todos={state} />
+        </div>
+      </Context.Provider>
+    );
 }
-
-export default App;
